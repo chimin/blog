2991ea4064754527c7fc3e58e95a75f0fb2c4129
chimin <fantazier@hotmail.com>
2021-01-29 22:17:06 +0800

    Enable Selenium in Jasmine test

--- src/specs/test.spec.ts
+++ src/specs/test.spec.ts
@@ -1,5 +1,19 @@
+import { writeFileSync } from 'fs';
+import * as webdriver from 'selenium-webdriver';
+import * as chrome from 'selenium-webdriver/chrome';
+import * as chromedriver from 'chromedriver';
+
+chrome.setDefaultService(new chrome.ServiceBuilder(chromedriver.path).build());
+
 describe('test', () => {
-    it('should fail', () => {
-        expect(1).toBe(0);
+    it('should pass', async () => {
+        const driver = await new webdriver.Builder().forBrowser('chrome').build();
+        try {
+            await driver.get('http://localhost:3000');
+            const screenshot = await driver.takeScreenshot();
+            writeFileSync('screenshot.png', Buffer.from(screenshot, 'base64'));
+        } finally {
+            await driver.quit();
+        }
     });
 });
