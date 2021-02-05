14819bd62f822dd95ed08ff92550cd92b1e8088a
chimin <fantazier@hotmail.com>
2021-02-05 23:16:39 +0800

    Hide unpublished posts

--- src/app/posts.ts
+++ src/app/posts.ts
@@ -1,11 +1,16 @@
 import fs from 'fs';
 import path from 'path';
+import posts from '../data/posts.json';
 
 export function listPosts() {
-    return require('../data/posts.json');
+    return posts.filter(post => post.publishDate);
 }
 
 export function getPostContent(id: unknown) {
+    if (!posts.find(post => post.id == id && post.publishDate)) {
+        return;
+    }
+
     const filePath = path.join('src/data/posts', `${id}.html`);
     return fs.readFileSync(filePath, 'utf8');
 }
